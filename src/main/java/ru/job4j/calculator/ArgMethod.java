4547f5392d5006243fb58a67507068acc97020b3
chykh <chykh@mail.ru>
2021-09-10 15:28:10 +0300

    3.1. Загрузка файлов на gitпhub [#243569]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -5,6 +5,10 @@ public class ArgMethod {
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
+    public static int func1(int x) {
+        return x * x + 1;
+    }
+
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 33;
@@ -12,5 +16,7 @@ public class ArgMethod {
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
+        int result = ArgMethod.func1(100);
+        System.out.println(result);
     }
 }
