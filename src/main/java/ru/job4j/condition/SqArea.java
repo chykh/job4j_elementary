447dec9306fa3c430ea1f5c60afc1ee0b4d2f298
chykh <chykh@mail.ru>
2021-09-16 12:51:16 +0300

    3.1. Загрузка файлов на gitпhub [#243569]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -2,16 +2,17 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-        double rsl = p / (2 * (k + 1));
-        return rsl;
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double rls = l * h;
+        return rls;
     }
 
     public static void main(String[] args) {
         int k = 2;
         int p = 6;
-        double h = SqArea.square(p, k);
-        double l = h * k;
-        double s = l * h;
-        System.out.println(" p = 6, k = 2 / h = " + h + ", l = " + l + ", s = " + s);
+        double real = SqArea.square(p, k);
+
+        System.out.println(" p = 6, k = 2 , s = " + 2 + ", real  = " + real);
     }
 }
