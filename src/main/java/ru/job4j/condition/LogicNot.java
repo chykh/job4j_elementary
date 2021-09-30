21a2db57f14f8d10c9495c776968842ef0570fac
chykh <chykh@mail.ru>
2021-09-30 15:44:07 +0300

    4.3.3. Логическое отрицание ! [#334072]

---
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,46 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return !isEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return !isPositive(num) || isEven(num);
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(LogicNot.isEven(a));
+        System.out.println(LogicNot.isPositive(a));
+        System.out.println(LogicNot.notEven(a));
+        System.out.println(LogicNot.notPositive(a));
+        System.out.println(LogicNot.notEvenAndPositive(a));
+        System.out.println(LogicNot.evenOrNotPositive(a));
+
+        a = -6;
+        System.out.println(LogicNot.isEven(a));
+        System.out.println(LogicNot.isPositive(a));
+        System.out.println(LogicNot.notEven(a));
+        System.out.println(LogicNot.notPositive(a));
+        System.out.println(LogicNot.notEvenAndPositive(a));
+        System.out.println(LogicNot.evenOrNotPositive(a));
+    }
+}
