9a9425e5eee130db0c3057e9e166742089980bdd
chykh <chykh@mail.ru>
2021-10-14 19:14:24 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -1,5 +1,19 @@
 package ru.job4j.loop;
 
+/*
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(0, 5));
+        System.out.println(sum(1, 49));
+
+        System.out.println(Counter.sumByEven(0, 10));
+        System.out.println(Counter.sumByEven(3, 8));
+        System.out.println(Counter.sumByEven(1, 1));
+    }
+ */
+
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
@@ -9,11 +23,13 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(0, 5));
-        System.out.println(sum(1, 49));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int counter = start; counter <= finish; counter++) {
+            if (counter % 2 == 0) {
+                sum = sum + counter;
+            }
+        }
+        return sum;
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -12,4 +12,20 @@ public class CounterTest {
         int expected = 14;
         Assert.assertEquals(14, Counter.sum(start, finish));
     }
+
+    @Test
+    public void when0and10sumByEven30() {
+        int start = 0;
+        int finish = 10;
+        int expected = 30;
+        Assert.assertEquals(expected, Counter.sumByEven(start, finish));
+    }
+
+    @Test
+    public void when10and16sumByEven52() {
+        int start = 10;
+        int finish = 16;
+        int expected = 52;
+        Assert.assertEquals(expected, Counter.sumByEven(start, finish));
+    }
 }
