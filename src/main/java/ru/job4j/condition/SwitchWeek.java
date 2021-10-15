063809785840789833d45af09e7377a952a9f1cf
chykh <chykh@mail.ru>
2021-10-15 17:24:44 +0300

    4.6.1. Switch [#334070 #213828]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class SwitchWeek {
 
     public static String nameOfDay(int day) {
-        String name = "Ошибка";
+        String name;
         switch (day) {
             case 1:
                 name = "Понедельник";
@@ -27,6 +27,7 @@ public class SwitchWeek {
                 name = "Воскресенье";
                 break;
             default:
+                name = "Ошибка";
         }
         return name;
     }
