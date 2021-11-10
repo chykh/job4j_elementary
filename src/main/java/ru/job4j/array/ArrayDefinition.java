75dbb41815b84716c5373fdc744bdfa081850def
chykh <chykh@mail.ru>
2021-11-10 17:53:17 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,10 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Gosha";
+        names[1] = "Sasha";
+        names[2] = "Dasha";
+        names[3] = "Misha";
     }
 }
